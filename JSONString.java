649d0beba569db2b982c3434947489d9428c800c
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2023-11-16 19:08:51 -0600

    Add JSONString.

---
+++ JSONString.java
@@ -0,0 +1,71 @@
+import java.io.PrintWriter;
+
+/**
+ * JSON strings.
+ */
+public class JSONString {
+
+  // +--------+------------------------------------------------------
+  // | Fields |
+  // +--------+
+
+  /**
+   * The underlying string.
+   */
+  String value;
+
+  // +--------------+------------------------------------------------
+  // | Constructors |
+  // +--------------+
+
+  /**
+   * Build a new JSON string for a particular string.
+   */
+  public JSONString(String value) {
+    this.value = value;
+  } // JSONString(String)
+
+  // +-------------------------+-------------------------------------
+  // | Standard object methods |
+  // +-------------------------+
+
+  /**
+   * Convert to a string (e.g., for printing).
+   */
+  public String toString() {
+    return "";          // STUB
+  } // toString()
+
+  /**
+   * Compare to another object.
+   */
+  public boolean equals(Object other) {
+    return true;        // STUB
+  } // equals(Object)
+
+  /**
+   * Compute the hash code.
+   */
+  public int hashCode() {
+    return 0;           // STUB
+  } // hashCode()
+
+  // +--------------------+------------------------------------------
+  // | Additional methods |
+  // +--------------------+
+
+  /**
+   * Write the value as JSON.
+   */
+  public void writeJSON(PrintWriter pen) {
+                        // STUB
+  } // writeJSON(PrintWriter)
+
+  /**
+   * Get the underlying value.
+   */
+  public String getValue() {
+    return this.value;
+  } // getValue()
+
+} // class JSONString
