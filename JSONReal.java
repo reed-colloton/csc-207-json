04e62edfb347339d6854f3017e1359aad21c2553
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2023-11-16 19:24:30 -0600

    Add reals.

---
+++ JSONReal.java
@@ -0,0 +1,86 @@
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+
+/**
+ * JSON reals.
+ */
+public class JSONReal {
+
+  // +--------+------------------------------------------------------
+  // | Fields |
+  // +--------+
+
+  /**
+   * The underlying double.
+   */
+  BigDecimal value;
+
+  // +--------------+------------------------------------------------
+  // | Constructors |
+  // +--------------+
+
+  /**
+   * Create a new real given the underlying string.
+   */
+  public JSONReal(String str) {
+    this.value = new BigDecimal(str);
+  } // JSONReal(String)
+
+  /**
+   * Create a new real given a BigDecimal.
+   */
+  public JSONReal(BigDecimal value) {
+    this.value = value;
+  } // JSONReal(BigDecimal)
+
+  /**
+   * Create a new real given a double.
+   */
+  public JSONReal(double d) {
+    this.value = BigDecimal.valueOf(d);
+  } // JSONReal(double)
+
+  // +-------------------------+-------------------------------------
+  // | Standard object methods |
+  // +-------------------------+
+
+  /**
+   * Convert to a string (e.g., for printing).
+   */
+  public String toString() {
+    return this.value.toString();
+  } // toString()
+
+  /**
+   * Compare to another object.
+   */
+  public boolean equals(Object other) {
+    return true;        // STUB
+  } // equals(Object)
+
+  /**
+   * Compute the hash code.
+   */
+  public int hashCode() {
+    return 0;           // STUB
+  } // hashCode()
+
+  // +--------------------+------------------------------------------
+  // | Additional methods |
+  // +--------------------+
+
+  /**
+   * Write the value as JSON.
+   */
+  public void writeJSON(PrintWriter pen) {
+                        // STUB
+  } // writeJSON(PrintWriter)
+
+  /**
+   * Get the underlying value.
+   */
+  public BigDecimal getValue() {
+    return this.value;
+  } // getValue()
+
+} // class JSONReal
