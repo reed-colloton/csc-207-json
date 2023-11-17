1b8c5c636df4b8188e63d0c934ae17b9e4c19b81
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2023-11-16 20:16:45 -0600

    Update the parser skeleton.

--- JSON.java
+++ JSON.java
@@ -1,10 +1,22 @@
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
 import java.text.ParseException;
 
 /**
  * Utilities for our simple implementation of JSON.
  */
 public class JSON {
+  // +---------------+-----------------------------------------------
+  // | Static fields |
+  // +---------------+
+
+  /**
+   * The current position in the input.
+   */
+  static int pos;
+
   // +----------------+----------------------------------------------
   // | Static methods |
   // +----------------+
@@ -12,14 +24,67 @@ public class JSON {
   /**
    * Parse a string into JSON.
    */
-  public static JSONValue parse(String source) throws ParseException {
-    throw new ParseException("Unimplemented", 0);
+  public static JSONValue parse(String source) throws ParseException, IOException {
+    return parse(new StringReader(source));
   } // parse(String)
 
   /**
    * Parse a file into JSON.
    */
   public static JSONValue parseFile(String filename) throws ParseException, IOException {
-    throw new IOException("Unimplemented");
+    FileReader reader = new FileReader(filename);
+    JSONValue result = parse(reader);
+    reader.close();
+    return result;
   } // parseFile(String)
+
+  /**
+   * Parse JSON from a reader.
+   */
+  public static JSONValue parse(Reader source) throws ParseException, IOException {
+    pos = 0;
+    JSONValue result = parseKernel(source);
+    if (-1 != skipWhitespace(source)) {
+      throw new ParseException("Characters remain at end", pos);
+    }
+    return result;
+  } // parse(Reader)
+
+  // +---------------+-----------------------------------------------
+  // | Local helpers |
+  // +---------------+
+
+  /**
+   * Parse JSON from a reader, keeping track of the current position
+   */
+  static JSONValue parseKernel(Reader source) throws ParseException, IOException {
+    int ch;
+    ch = skipWhitespace(source);
+    if (-1 == ch) {
+      throw new ParseException("Unexpected end of file", pos);
+    }
+    // STUB
+    throw new ParseException("Unimplemented", pos);
+  } // parseKernel
+
+  /**
+   * Get the next character from source, skipping over whitespace.
+   */
+  static int skipWhitespace(Reader source) throws IOException {
+    int ch;
+    do {
+      ch = source.read();
+      ++pos;
+    } while (isWhitespace(ch));
+    return ch;
+  } // skipWhitespace(Reader)
+
+  /**
+   * Determine if a character is JSON whitespace (newline, carriage return,
+   * space, or tab).
+   */
+  static boolean isWhitespace(int ch) {
+    return (' ' == ch) || ('\n' == ch) || ('\r' == ch) || ('\t' == ch);
+  } // isWhiteSpace(int)
+
 } // class JSON
