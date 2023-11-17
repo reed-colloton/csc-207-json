c7c24e4696f1c7594bb174c47ee5daa679ecf0c5
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2023-11-16 19:39:24 -0600

    Add arrays.

---
+++ JSONArray.java
@@ -0,0 +1,111 @@
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * JSON arrays.
+ */
+public class JSONArray {
+
+  // +--------+------------------------------------------------------
+  // | Fields |
+  // +--------+
+
+  /**
+   * The underlying array.
+   */
+  ArrayList<JSONValue> values;
+
+  // +--------------+------------------------------------------------
+  // | Constructors |
+  // +--------------+
+
+  /**
+   * Build a new array.
+   */
+  public JSONArray() {
+    this.values = new ArrayList<JSONValue>();
+  } // JSONArray() 
+
+  // +-------------------------+-------------------------------------
+  // | Standard object methods |
+  // +-------------------------+
+
+  /**
+   * Convert to a string (e.g., for printing).
+   */
+  public String toString() {
+    return "";          // STUB
+  } // toString()
+
+  /**
+   * Compare to another object.
+   */
+  public boolean equals(Object other) {
+    return true;        // STUB
+  } // equals(Object)
+
+  /**
+   * Compute the hash code.
+   */
+  public int hashCode() {
+    return 0;           // STUB
+  } // hashCode()
+
+  // +--------------------+------------------------------------------
+  // | Additional methods |
+  // +--------------------+
+
+  /**
+   * Write the value as JSON.
+   */
+  public void writeJSON(PrintWriter pen) {
+                        // STUB
+  } // writeJSON(PrintWriter)
+
+  /**
+   * Get the underlying value.
+   */
+  public ArrayList<JSONValue> getValue() {
+    return this.values;
+  } // getValue()
+
+  // +---------------+-----------------------------------------------
+  // | Array methods |
+  // +---------------+
+
+  /**
+   * Add a value to the end of the array.
+   */
+  public void add(JSONValue value) {
+    this.values.add(value);
+  } // add(JSONValue)
+
+  /**
+   * Get the value at a particular index.
+   */
+  public JSONValue get(int index) throws IndexOutOfBoundsException {
+    return this.values.get(index);
+  } // get(int)
+
+  /**
+   * Get the iterator for the elements.
+   */
+  public Iterator<JSONValue> iterator() {
+    return this.values.iterator();
+  } // iterator()
+
+  /**
+   * Set the value at a particular index.
+   */
+  public void set(int index, JSONValue value) throws IndexOutOfBoundsException {
+    this.values.set(index, value);
+  } // set(int, JSONValue)
+
+  /**
+   * Determine how many values are in the array.
+   */
+  public int size() {
+    return this.values.size();
+  } // size()
+} // class JSONArray
