7500c443901772169fe5acd824d4e55373644659
nmendola13 <144048525+nmendola13@users.noreply.github.com>
2023-12-03 16:46:38 -0600

    Add files via upload
    main with tests

--- src/HashTableExperiments.java
+++ src/HashTableExperiments.java
@@ -12,8 +12,22 @@ public class HashTableExperiments {
 
   public static void main(String[] args) throws ParseException, IOException {
     PrintWriter pen = new PrintWriter(System.out, true);
-    JSONValue fileJSONs = JSON.parseFile("src/tests/test.json");
-    pen.println("Retrieved Value:\n" + fileJSONs);
+    pen.println("running");
+    JSONValue fileJSON = JSON.parseFile("/home/mendolan/CSC207/miniprojects/csc-207-json/src/tests/test.json");
+    pen.println("Retrieved Value" + fileJSON.getValue());
+
+    JSONValue fileJSON2 = JSON.parseFile("/home/mendolan/CSC207/miniprojects/csc-207-json/src/tests/test.json");
+    pen.println("Retrieved Value" + fileJSON2.getValue());
+
+    JSONValue fileJSON3 = JSON.parseFile("/home/mendolan/CSC207/miniprojects/csc-207-json/src/tests/test.json");
+    pen.println("Retrieved Value" + fileJSON3.getValue());
+
+    JSONValue fileJSON4 = JSON.parseFile("/home/mendolan/CSC207/miniprojects/csc-207-json/src/tests/test.json");
+    pen.println("Retrieved Value" + fileJSON4.getValue());
+
+    JSONValue fileJSON5 = JSON.parseFile("/home/mendolan/CSC207/miniprojects/csc-207-json/src/tests/test.json");
+    pen.println("Retrieved Value" + fileJSON5.getValue());
+
   } //main(PrintWriter)
 
 } // class HashTableExpt
